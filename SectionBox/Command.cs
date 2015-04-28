09f76d67e54bca6ca3ff495a6181db4127acfec6
2adicted <dido.nenov@gmail.com>
2015-04-28 22:40:21 +0100

    Bugged?

--- SectionBox/Command.cs
+++ SectionBox/Command.cs
@@ -384,25 +384,33 @@ namespace SectionBox
 
             Selection sel = uidoc.Selection;
 
+            //performCheck(doc, message);
 
             var form_to_show = new LoadExcel(uiapp);
 
             form_to_show.ShowDialog();
 
-            
-            View3D view = doc.ActiveView as View3D;
 
+            View3D view = Get3dView(doc);
+            
             if (null == view)
             {
                 message = "Please run this command in a 3D view.";
                 return Result.Failed;
             }
 
+            uidoc.ActiveView = view;
+
             Element scopeBox
               = new FilteredElementCollector(doc, view.Id)
                 .OfCategory(BuiltInCategory.OST_VolumeOfInterest)
                 .WhereElementIsNotElementType()
                 .FirstElement();
+            if (null == scopeBox)
+            {
+                message = "Please make sure you have at least one ScopeBox in the project.";
+                return Result.Failed;
+            }
             ElementId scopeBoxName = scopeBox.Id;
             
             BoundingBoxXYZ viewScopeBox
@@ -430,6 +438,43 @@ namespace SectionBox
             }
 
             return Result.Succeeded;
+        }
+
+
+        private void performCheck(Document doc, string message)
+        {
+            View3D view = doc.ActiveView as View3D;
+            if(null == view)
+            {
+                message = "Please run this command in a 3D view.";
+            }
+        }
+
+        /// <summary>
+        /// Retrieve a suitable 3D view from document.
+        /// </summary>
+        View3D Get3dView(Document doc)
+        {
+            FilteredElementCollector collector
+              = new FilteredElementCollector(doc)
+                .OfClass(typeof(View3D));
+
+            foreach (View3D v in collector)
+            {
+                Debug.Assert(null != v,
+                  "never expected a null view to be returned"
+                  + " from filtered element collector");
+
+                // Skip view template here because view 
+                // templates are invisible in project 
+                // browser
+
+                if (!v.IsTemplate)
+                {
+                    return v;
+                }
+            }
+            return null;
         }
 
     }
